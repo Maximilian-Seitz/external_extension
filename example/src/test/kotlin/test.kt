772db095dd2af922bd1fd6cd878e1405c4a6235f
Maximilian Seitz <maximilian.seitz@stud.th-deg.de>
2020-07-27 17:23:53 +0200

    Add test case for external extension function with arguments

--- example/src/test/kotlin/test.kt
+++ example/src/test/kotlin/test.kt
@@ -5,21 +5,23 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AnnotationTest {
-	@Test fun testSimple() {
-		val unitVectors = listOf(
-				Vector1D(1f),
-				Vector2D(1f, 1f),
-				Vector3D(1f, 1f, 1f)
-		)
+	private val unitVectors = mapOf(
+			1 to Vector1D(1f),
+			2 to Vector2D(1f, 1f),
+			3 to Vector3D(1f, 1f, 1f)
+	)
+	
+	@Test fun simpleTest() {
+		assertEquals(unitVectors[1]?.inverse(), Vector1D(-1f))
+		assertEquals(unitVectors[2]?.inverse(), Vector2D(-1f, -1f))
+		assertEquals(unitVectors[3]?.inverse(), Vector3D(-1f, -1f, -1f))
+	}
+	
+	@Test fun parameterTest() {
+		val x = 3f
 		
-		assertEquals(Vector1D(-1f), unitVectors[0].inverse())
-		assertEquals(Vector2D(-1f, -1f), unitVectors[1].inverse())
-		assertEquals(Vector3D(-1f, -1f, -1f), unitVectors[2].inverse())
-		
-		//Just for a nice output
-		unitVectors.forEach {
-			println(it)
-			println(it.inverse())
-		}
+		assertEquals(unitVectors[1]?.scale(x), Vector1D(x))
+		assertEquals(unitVectors[2]?.scale(x), Vector2D(x, x))
+		assertEquals(unitVectors[3]?.scale(x), Vector3D(x, x, x))
 	}
 }
